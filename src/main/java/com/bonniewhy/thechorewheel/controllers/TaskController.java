c33d29f0c30181a072ab0f1e5d25a419bd495776
bonniewhy <bonnieinpixels@gmail.com>
2019-04-24 08:18:54 -0500

    Everything up to date so I can switch branches.

--- src/main/java/com/bonniewhy/thechorewheel/controllers/TaskController.java
+++ src/main/java/com/bonniewhy/thechorewheel/controllers/TaskController.java
@@ -1,4 +1,37 @@
 package com.bonniewhy.thechorewheel.controllers;
 
+import com.bonniewhy.thechorewheel.models.data.RoomDao;
+import com.bonniewhy.thechorewheel.models.data.TaskDao;
+import com.bonniewhy.thechorewheel.models.data.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class TaskController {
+
+    @Autowired
+    RoomDao roomDao;
+
+    @Autowired
+    TaskDao taskDao;
+
+    @Autowired
+    UserDao userDao;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String displayTasks(Model model) {
+
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String addTasks(Model model) {
+
+    }
+
+
+
+
 }
